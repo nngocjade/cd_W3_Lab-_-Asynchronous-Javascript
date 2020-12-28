f95bcb6a0d8200f738a997bfd9d958ced5177843
nngocjade <nngocjade@gmail.com>
2020-12-28 17:48:08 +0700

    finished for now

--- script.js
+++ script.js
@@ -7,62 +7,98 @@ let recordButton = document.getElementById("record-button");
 let recorder = null;
 
 // an array that holds all the recordings
-let trunks = [];
+let chunks = [];
 
 const main = async () => {
   let stream = await navigator.mediaDevices.getUserMedia({ audio: true }); // we only need audio
   recorder = new MediaRecorder(stream);
 
-  // event listeners
+  // record and stop event listeners
   recordButton.addEventListener("click", startRecording);
   stopButton.addEventListener("click", stopRecording);
+
+  // window.addEventListener("click", outSideClick);
+
+  // save data once available
+  recorder.ondataavailable = saveCurrentRecording;
+
+  // send to media player
+  recorder.onstop = sendToMediaPlayer;
 };
 
 //Functions
 const startRecording = () => {
-  if ((recordButton.disabled = true)) {
-    recorder.start();
-    recordButton.style.backgroundColor = "rgba(242, 242, 242, .5)";
-    recordButton.style.border = "1px solid red";
-    recordButton.style.color = "red";
-    recordButton.style.transition = ".2s";
-    recordButton.style.width = "3.5rem";
-    recordButton.style.height = "3.5rem";
-  }
-  // if ((stopButton.disabled = true)) {
-  //   stopButton.style.backgroundColor = "#f1f3f4";
-  //   stopButton.style.border = "none";
-  // }
-  recorderButton.disabled = false;
+  recorder.start();
+  recordButton.disabled = true;
+  recordButton.style.backgroundColor = "rgba(242, 242, 242, .5)";
+  recordButton.style.border = "1px solid red";
+  recordButton.style.color = "red";
+  recordButton.style.transition = ".2s";
+  recordButton.style.width = "3.5rem";
+  recordButton.style.height = "3.5rem";
+
+  collapsePlayer();
+
+  stopButton.disabled = false;
+  stopButton.style.backgroundColor = "#f1f3f4";
+  stopButton.style.border = "none";
+  stopButton.style.color = "rgb(158, 158, 158)";
 };
 
 // console.log(startRecording());
 
 const stopRecording = () => {
-  if ((stopButton.disabled = true)) {
-    recorder.stop();
-    stopButton.style.backgroundColor = "rgba(242, 242, 242, .5)";
-    stopButton.style.border = "1px solid red";
-    stopButton.style.color = "red";
-    stopButton.style.transition = ".2s";
-    stopButton.style.width = "3.5rem";
-    stopButton.style.height = "3.5rem";
-  }
-  if ((recordButton.disabled = true)) {
-    recordButton.style.backgroundColor = "#f1f3f4";
-    recordButton.style.border = "none";
-    recordButton.style.color = "rgb(158, 158, 158)";
-  }
+  recorder.stop();
+  stopButton.disabled = true;
+  stopButton.style.backgroundColor = "rgba(242, 242, 242, .5)";
+  stopButton.style.border = "1px solid red";
+  stopButton.style.color = "red";
+  stopButton.style.transition = ".2s";
+  stopButton.style.width = "3.5rem";
+  stopButton.style.height = "3.5rem";
+
+  recordButton.disabled = false;
+  recordButton.style.backgroundColor = "#f1f3f4";
+  recordButton.style.border = "none";
+  recordButton.style.color = "rgb(158, 158, 158)";
+
   expandPlayer();
 };
 
 function expandPlayer() {
-  let audio = document.getElementById("audio");
-  audio.style.width = "45%";
+  audioElement.style.width = "45%";
 }
 function collapsePlayer() {
-  let audio = document.getElementById("audio");
-  audio.style.width = "0%";
+  audioElement.style.width = "0%";
 }
+
+const saveCurrentRecording = (event) => {
+  chunks.push(event.data);
+  console.log(chunks);
+};
+
+const sendToMediaPlayer = () => {
+  const blob = new Blob(chunks, {
+    type: "audio/mp4; codecs=opus",
+  });
+  const url = URL.createObjectURL(blob);
+  audioElement.setAttribute("src", url);
+
+  //clear the recorded chunks if preferred
+  // chunks = [];
+};
+
+// function outSideClick(e) {
+//   if (e.target != recordButton || e.target != stopButton || audioElement) {
+//     recordButton.disabled = false;
+//     recordButton.style.backgroundColor = "#f1f3f4";
+//     recordButton.style.border = "none";
+//     recordButton.style.color = "rgb(158, 158, 158)";
+//     stopButton.disabled = false;
+//     stopButton.style.backgroundColor = "#f1f3f4";
+//     stopButton.style.border = "none";
+//     stopButton.style.color = "rgb(158, 158, 158)";
+//   }
+// }
 
 main();
